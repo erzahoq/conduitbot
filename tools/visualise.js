3ae68c9a1c3e4398e0bd639cf1945e8172851384
erzahoq <154752392+erzahoq@users.noreply.github.com>
2026-01-09 23:30:52 -0800

    whoops

--- tools/visualise.js
+++ tools/visualise.js
@@ -178,21 +178,21 @@ const graph = buildSubgraph(markovChain, seedKeys, {
 });
 
 fs.writeFileSync(
-  path.join(__dirname, '..', 'docs', 'graph.json'),
+  path.join(__dirname, '..', 'markov', 'graph.json'),
   JSON.stringify(graph, null, 2),
   'utf8'
 );
 
-console.log(`[export] wrote docs/graph.json (${graph.nodes.length} nodes, ${graph.edges.length} edges)`);
+console.log(`[export] wrote markov/graph.json (${graph.nodes.length} nodes, ${graph.edges.length} edges)`);
 
 
 const nextIndex = buildNextIndex(markovChain, 80);
 
 fs.writeFileSync(
-  path.join(__dirname, '..', 'docs', 'next_index.json'),
+  path.join(__dirname, '..', 'markov', 'next_index.json'),
   JSON.stringify({ order, nextIndex }, null, 2),
   'utf8'
 );
 
-console.log(`[export] wrote docs/next_index.json (${Object.keys(nextIndex).length} keys)`);
+console.log(`[export] wrote markov/next_index.json (${Object.keys(nextIndex).length} keys)`);
 
